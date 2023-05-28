0f442e3ea7ff752d07f061f2240893ed85153bd4
leonel <c.leonelortiz10@gmail.com>
2023-05-28 16:38:02 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  // var a = toLowerCase(7)
+  charAtPosition()
+  // console.log(a)
 
   return (
     <div className="container-xxl">       
