3a4645456a93713a120f7d813af90c327717453e
leonel <c.leonelortiz10@gmail.com>
2023-07-07 13:52:57 -0300

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -24,7 +24,7 @@ function About() {
                     Soy Leonel, un profesional con formación en desarrollo de software y estudiante de ingeniería en sistemas de información en UTN, tengo 23 años y vivo en Tucumán, Argentina. Del lado del frontend he trabajado como diseñador, maquetador y desarrollador, y del lado backend he trabajado principalmente creando API. Estoy para sumar mis conocimientos a un equipo de trabajo y siga desarrollando mis habilidades como profesional.
                     <img src={arg} className='ms-2' style={{height:"25px"}}/>
                     </p>
-                    <a className="btn btn-blackgreen d-block ms-auto me-4" href={`https://www.dropbox.com/scl/fi/p6p028c25b1aoh4ynw60v/cv-OrtizCarlosLeonel.pdf?rlkey=tbxmxfrtpvpjyni9nuhres8p3&dl=0`} target='_blank'>Donwload CV</a>
+                    <a className="btn btn-blackgreen d-block ms-auto me-4" href={`https://www.dropbox.com/scl/fi/0zvtv7n5850heu4f1evjv/cv-OrtizCarlosLeonel.pdf?rlkey=i78tbnelpj7wuf2fj23nt2kwc&dl=0`} target='_blank'>Donwload CV</a>
             </div>
             <div className="col-lg-5 card-blur rounded text-center my-4 my-lg-0 p-1 animation-show ">
                 <h1>My skills</h1>
