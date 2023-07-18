b3adb24b4a70cded2805498db118ffd6a899d55a
leonel <c.leonelortiz10@gmail.com>
2023-07-18 15:23:41 -0300

    reduce perfil

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import perfil from '../assets/perfil4.png'
+import perfil from '../assets/perfil4.webp'
 function Home() {
     return (
         <div className={"container-fluid body-home  d-flex flex-column flex-sm-row  justify-content-center align-items-center py-3 m-auto h-100 w-100 animation-show"}>
